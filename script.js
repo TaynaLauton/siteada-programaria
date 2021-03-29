ed90e3b2e5115d76d3ddb40651fa004e9aa8c6da
TaynaLauton <taynalauton@gmail.com>
2021-03-29 16:21:19 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById("botaoEnviar").addEventListener("click", validaFormulario)
+
+function validaFormulario(){
+    if(document.getElementById("nome").value != "" && document.getElementById("email").value != "" && document.getElementById("telefone").value != ""){
+        alert("Dados enviados com sucesso!")
+    }
+    else{
+        alert("Por favor preencha os campos nome e email!")
+    }
+}
